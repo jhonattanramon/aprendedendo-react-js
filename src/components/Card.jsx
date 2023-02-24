a9a3d2954f759bd2cb61a079d5a173668f655004
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-02-24 15:29:17 -0300

    .

---
+++ src/components/Card.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Card = () => {
+
+    return(
+
+        <>
+
+        <div>
+            <img src="" alt="" />
+            <h1> </h1>
+            <p></p>
+            
+        </div>
+        
+        </>
+    )
+}
+
+export default Card;
